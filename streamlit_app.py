473fa4086bb1f5b113e1a7b2e2fb6fdc540fdfd4
Taladhak <114437926+Taladhak@users.noreply.github.com>
2022-09-26 13:37:58 -0600

    Update streamlit_app.py
    Added new text like and header
    Added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit
 streamlit.title(' My parentsNew Healthy Dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Egg')
+streamlit.text('🐟Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥚Hard-Boiled Egg')
+streamlit.text('🥑🥪 Avocado Toast')
+
+
+
+streamlit.header('🍌🥭Build Your Own Fruit Smoothie🥝🍇')
